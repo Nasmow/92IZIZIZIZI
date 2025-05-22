5af1d3e51dfc81f49ea7e0e61e4f034e17376ee6
Nasmo <89615630+Nasmow@users.noreply.github.com>
2025-05-22 05:02:19 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/92IZIZIZIZI/', // Nom de votre repository GitHub
+  base: '/', // Pour domaine personnalisé bagagekom.com
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
